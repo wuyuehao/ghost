e2410c06bac6e02d9380ef0ca3931c3a2b493d25
tonywu <tonywu@paypal.com>
2018-09-08 12:58:35 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='ghost',
   packages=['ghost'],
-  version='0.15',
+  version='0.16',
   description='Configurable Machine Learning Library',
   author='Tony Wu',
   author_email='wuyuehao@gmail.com',
   url='https://github.com/wuyuehao/ghost', 
-  download_url= 'https://github.com/wuyuehao/ghost/tarball/0.15',
+  download_url= 'https://github.com/wuyuehao/ghost/tarball/0.16',
   keywords = ['AI', 'ML'],
   classifiers = [],
 ) 
