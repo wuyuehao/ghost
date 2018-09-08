1b37afbcb8dab75c812b3e038d840deb17a287de
tonywu <tonywu@paypal.com>
2018-09-08 12:55:42 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author='Tony Wu',
   author_email='wuyuehao@gmail.com',
   url='https://github.com/wuyuehao/ghost', 
-  download_url= 'https://github.com/wuyuehao/repo/ghost/',
+  download_url= 'https://github.com/wuyuehao/ghost/tarball/0.14',
   keywords = ['AI', 'ML'],
   classifiers = [],
 )      zip_safe=False)
